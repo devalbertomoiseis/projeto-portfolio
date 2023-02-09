a31e36a9f7571158c17dee5d5e2411a58a775b57
albertomoiseis <contatoalbertomoiseis@gmail.com>
2023-02-09 16:01:15 -0300

    Efeito Maquina de Escrever

--- js/javascript.js
+++ js/javascript.js
@@ -32,3 +32,18 @@ navTabs.forEach((nav, index) => {
     tabPane[index].classList.add("active");
   });
 });
+
+function typewriterEffect(element, delay = 0) {
+  let split_element = element.innerHTML.split("");
+  element.innerHTML = "";
+  setTimeout(() => {
+    split_element.forEach((value, index) => {
+      setTimeout(() => {
+        element.innerHTML += value;
+      }, 65 * index);
+    });
+  }, delay);
+}
+
+typewriterEffect(document.querySelector("h1"));
+typewriterEffect(document.querySelector("h1"), 900);
